ade7f819736ed63157745477b5a400c96332930f
Tharel <tharel.biznes@gmail.com>
2022-02-20 21:45:13 +0100

    chase player as chase target

--- Assets/Code/Zombie/DataComponents/TargetPositionData.cs
+++ Assets/Code/Zombie/DataComponents/TargetPositionData.cs
@@ -3,6 +3,7 @@ using Unity.Mathematics;
 
 namespace Code.Zombie.DataComponents
 {
+    [GenerateAuthoringComponent]
     public struct TargetPositionData : IComponentData
     {
         public float3 Value;
--- Assets/Code/Zombie/Systems/ChasePlayerSystem.cs
+++ Assets/Code/Zombie/Systems/ChasePlayerSystem.cs
@@ -1,8 +1,6 @@
 using Code.Zombie.DataComponents;
 using Code.Zombie.Tags;
-using Unity.Collections;
 using Unity.Entities;
-using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
@@ -10,31 +8,21 @@ namespace Code.Zombie.Systems
 {
     public class ChasePlayerSystem : SystemBase
     {
-        // private EndSimulationEntityCommandBufferSystem _endSimulationEntityCommandBufferSystem;
-        private EntityQuery _playerQuery;
-
-        protected override void OnCreate()
-        {
-            // _endSimulationEntityCommandBufferSystem =
-            //     World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
-            _playerQuery = GetEntityQuery(ComponentType.ReadOnly<PlayerTag>(), ComponentType.ReadOnly<Translation>());
-        }
-
         protected override void OnUpdate()
         {
-            // todo: fix leak errors, separate "serach for player" system and "move to target" system
-            var playerPosition = _playerQuery.ToComponentDataArrayAsync<Translation>(Allocator.TempJob, out JobHandle getPositionHandle);
             Entities
-                .WithReadOnly(playerPosition)
-                // .WithDisposeOnCompletion(playerPosition)
-                .ForEach((Entity enemyEntity, ref Translation enemyPosition, in SpeedData speedData, in EnemyTag enemyTag) =>
+                .ForEach((ref Translation enemyPosition, in SpeedData speedData, in EnemyTag enemyTag, in TargetPositionData targetPosition) =>
                 {
-                    var direction = math.normalize(playerPosition[0].Value - enemyPosition.Value);
-                    var currentPosition = enemyPosition.Value;
-                    enemyPosition.Value = new float3(
-                        currentPosition.x + direction.x * speedData.speed,
-                        0f,
-                        currentPosition.z + direction.z * speedData.speed);
+                    var vectorToTarget = targetPosition.Value - enemyPosition.Value;
+                    if (math.length(vectorToTarget) > CharacterUtility.StopMovingToTargetDistance)
+                    {
+                        var direction = math.normalize(vectorToTarget);
+                        enemyPosition.Value = new float3(
+                            enemyPosition.Value.x + direction.x * speedData.speed,
+                            0f,
+                            enemyPosition.Value.z + direction.z * speedData.speed);
+                    }
+                    
                 }).ScheduleParallel();
         }
     }
